dfd084dc93c3557142b760bbc44e30129f6c458e
maigre <thomas@37m.gr>
2020-09-28 22:26:04 +0200

    init

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
+
+var history = []
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  console.log('user connected')
+  
+  socket.emit('all messages', history)
+  
+  socket.on('chat message', function(msg) {
+    if (msg == '#clear') {
+      io.emit('clear');
+      history = []
+    }
+    else {
+      io.emit('chat message', msg);
+      history.push(msg)
+    }
   });
+
+  socket.on('disconnect', function(msg){
+    console.log('user disconnected')
+  });
+
 });
 
 http.listen(port, function(){
